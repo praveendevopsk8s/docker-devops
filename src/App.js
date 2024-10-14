66d9dfb3a3d76211826c7c827638dfc3afcdaaae
praveendevopsk8s <149125849+praveendevopsk8s@users.noreply.github.com>
2024-10-14 07:34:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: {process.env.REACT_APP_VERSION}
+          Application version: 1
       </p>
     </div>
   );
